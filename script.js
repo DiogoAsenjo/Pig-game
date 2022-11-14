cde99cd6fe58e848e8fd20902c1eb0bd2c7cbc10
Diogo Asenjo <diogoasenjo@outlook.com>
2022-11-14 11:23:08 -0300

    Switching players

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 'use strict';
 
 //Selecting elements. Aqui temos duas formas de selecionar id's do doc. HTML.
-let score0 = document.querySelector(`#score--0`);
+const player1 = document.querySelector(`.player--0`);
+const player2 = document.querySelector(`.player--1`);
+const score0 = document.querySelector(`#score--0`);
 const score1 = document.getElementById(`score--1`);
 const diceEl = document.querySelector(`.dice`);
-const currentScore0 = document.getElementById(`current--0`);
-const currentScore1 = document.getElementById(`current--1`);
+// const currentScore0 = document.getElementById(`current--0`);
+// const currentScore1 = document.getElementById(`current--1`);
 const rollDice = document.querySelector(`.btn--roll`);
 const newGame = document.querySelector(`.btn--new`);
 const hold = document.querySelector(`.btn--hold`);
@@ -14,9 +16,12 @@ const hold = document.querySelector(`.btn--hold`);
 score0.textContent = 0;
 score1.textContent = 0;
 diceEl.classList.add(`hidden`);
-currentScore0.textContent = 0;
-currentScore1.textContent = 0;
+// currentScore0.textContent = 0;
+// currentScore1.textContent = 0;
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0; //Player 1= 0 e Player 2= 1.
+player1.classList.add(`player--active`);
 
 //Rolling the dice.
 rollDice.addEventListener(`click`, function () {
@@ -26,8 +31,14 @@ rollDice.addEventListener(`click`, function () {
   console.log(dice);
   if (dice !== 1) {
     currentScore += dice;
-    currentScore0.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
-    //Switch to the next player
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player1.classList.toggle(`player--active`);
+    player2.classList.toggle(`player--active`);
   }
 });
